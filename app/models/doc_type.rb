00903e232365670019d7fa71aa5154d231330f32
Dmitry Volodin <neonik47.1@gmail.com>
2014-05-18 22:19:41 +0400

    Update doc_type.rb

--- app/models/doc_type.rb
+++ app/models/doc_type.rb
@@ -2,7 +2,7 @@ class DocType
   include Mongoid::Document
 
   field :name
-  field :is_deleted, type: Boolean, default: false
+  field :deleted, type: Boolean, default: false
 
   embeds_many :lines
   has_many :docs
